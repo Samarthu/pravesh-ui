d7c2f8285d83917c293c2a0987b0c1782dab4f0f
Gagan Gowda <gagan.gowda@elastic.run>
2022-04-04 23:16:06 +0530

    Docker commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,42 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+	// kit: {
+	// 	// hydrate the <div id="svelte"> element in src/app.html
 		
-		vite: {
-			ssr: {
-			  noExternal: [ 'dayjs' ]
-			}
-		  }
-	}
+	// 	vite: {
+	// 		ssr: {
+	// 		  noExternal: [ 'dayjs' ]
+	// 		}
+	// 	  }
+	// }
+
+	kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        browser: {
+            hydrate: true,
+            router: true
+          },      
+        paths: {
+            assets: '',
+            base: '/pravesh-ui'
+        },
+        adapter: adapter({
+            // default options are shown
+            // pages: '/roster-management',
+            // assets: '/roster-management',
+            fallback: "index.html",
+            // precompress: false
+
+        })
+        //,
+
+        // adapter:adapter({
+        //  fallback: "/home/akshay/new-sample/build/_app/pages/about.svelte-be298a1a.js"
+        // })
+
+    }
 };
 
 export default config;
